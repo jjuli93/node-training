82f3b449f68a52012b1d0ec232a02978d0d0425c
Pablo Ifrán <pabloifran@gmail.com>
2019-09-10 09:27:34 -0300

    Fix entrypoint not being recognized
    For some reason the entrypoint was not being executed when the container
    starts, rename it to just entrypoint and make some adjustments on the
    dockerfile for dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,11 @@ SEEDED_FILE=.seeded
 wait-for-it.sh db:5432 -t 15 -- echo "Database ready!"
 
 # Prepare the application
-npx knex migrate:latest 2>/dev/null
+echo "Running migrations..."
+npx knex migrate:latest
 
 if [ ! -f $SEEDED_FILE ]; then
+  echo "Seeding application..."
   npx knex seed:run
 
   touch $SEEDED_FILE
