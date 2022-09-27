1375dd44481f0628fde355cebfabfe9aac4c015c
Julián Ignacio Álvarez <alvarezjulianignacio@gmail.com>
2022-09-27 15:46:29 -0300

    removed unused code

--- src/lib/users/users.serializer.js
+++ src/lib/users/users.serializer.js
@@ -3,9 +3,7 @@ const { Serializer } = require('../../../lib');
 class UserSerializer extends Serializer {
   constructor() {
     super({ collectionName: 'users' });
-
     this.baseFields = [];
-
     this.meta = {};
   }
 }
